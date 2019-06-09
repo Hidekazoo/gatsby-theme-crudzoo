af9482b66ea509058fcd15ec1b83f93a9084b57a
Hidekazu Tanaka <tanakahidek@gmail.com>
2019-06-09 17:32:45 +0900

    update example

--- example/gatsby-config.js
+++ example/gatsby-config.js
@@ -1,7 +1,9 @@
 module.exports = {
   siteMetadata: {
+    language: `en`,
     title: `example`,
     author: `example`,
+    keywords: [`blog`, `gatsby`],
     description: `example site description`,
     siteUrl: `http://example.com`,
     social: {
