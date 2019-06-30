f2a4a6e9df728fe6ec70282aff21480f3eec5c25
Hidekazu Tanaka <tanakahidek@gmail.com>
2019-06-30 19:46:54 +0900

    export components

--- index.ts
+++ index.ts
@@ -1,3 +1,6 @@
-export { default as Header } from './src/components/header';
 export { default as Layout } from './src/components/layout';
 export { default as Box } from './src/components/box';
+export { default as SEO } from './src/components/seo';
+export { default as Bio } from './src/components/bio';
+export { default as ToggleDarkMode } from './src/components/toggleDarkMode';
+export { getLocalizedData, formatPostDate } from './src/utils/i18n';
