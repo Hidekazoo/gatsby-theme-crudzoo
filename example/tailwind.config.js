e11d91ddb663be636e5071194d460a648b4ba555
Hidekazu <tanakahidek@gmail.com>
2020-12-31 02:03:07 +0900

    update example

---
+++ example/tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  purge: [],
+  theme: {
+    fontFamily: {
+      sanSerif: ``,
+    },
+    extend: {
+      colors: {
+        primary: "#0091ea",
+        section: "#0091ea0f",
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+}
