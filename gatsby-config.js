770db0a34286f406188504b7b83e506ac00a3037
Hidekazu <tanakahidek@gmail.com>
2020-12-30 17:42:35 +0900

    fix plugin setting

--- gatsby-config.js
+++ gatsby-config.js
@@ -25,7 +25,7 @@ module.exports = ({}) => {
       options: {
         extensions: [`.mdx`, `.md`],
         defaultLayouts: {
-          default: require.resolve(`./src/components/layout.tsx`),
+          default: require.resolve(`./src/components/Layout.tsx`),
         },
         gatsbyRemarkPlugins: [
           {
